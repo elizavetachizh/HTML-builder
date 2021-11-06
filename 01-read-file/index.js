0f3e7da3c84c8dec6c08f1463696dad1202b0163
elizavetachizh <75884592+elizavetachizh@users.noreply.github.com>
2021-11-06 10:26:02 +0300

    feat: implement read-file, write-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,19 +1,20 @@
 const fs = require("fs");
 const path = require("path");
+
 // The firs tway
-const rr = fs.createReadStream(path.join(__dirname, "text.txt"), "utf-8");
-rr.on("readable", () => {
-  console.log(`readable: ${rr.read()}`);
-});
+// const rr = fs.createReadStream(path.join(__dirname, "text.txt"), "utf-8");
+// rr.on("readable", () => {
+//   console.log(`readable: ${rr.read()}`);
+// });
 
 //the second way
-fs.readFile(path.join(__dirname, "text.txt"), "utf-8", (err, data) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(data);
-});
+// fs.readFile(path.join(__dirname, "text.txt"), "utf-8", (err, data) => {
+//   if (err) {
+//     console.error(err);
+//     return;
+//   }
+//   console.log(data);
+// });
 
 //three way
 const { stdout } = require("process");
--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,24 +2,7 @@ const fs = require("fs");
 const readline = require("readline");
 const process = require("process");
 const path = require("path");
-
 const { stdout } = require("process");
-//Создать экземпляр readline.Interface
-
-//
-// const contect = "test";
-// fs.writeFile("2test.txt", contect, (err) => {
-//   if (err) {
-//     console.error(err);
-//     return;
-//   }
-//   //файл записан успешно
-// });
-
-// process.stdin.on("data", (data) => {
-//   fs.appendFile(path.join(__dirname, "2test.txt"), data.toString(), (err) => {
-//     if (err) throw err;
-//   });
 
 let filePath = path.join(__dirname, "test.txt");
 let writableStream = fs.createWriteStream(filePath);
@@ -29,14 +12,14 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question("Type your text\n", (answer) => {
+rl.question("Enter your text, please\n", (answer) => {
   rl.on("line", (input) => {
     if (input === "exit") {
       rl.close();
     }
   })
     .on("close", () => {
-      stdout.write("Bye");
+      stdout.write("Bye-Bye!");
     })
     .on("SIGINT", () => {
       rl.close();
