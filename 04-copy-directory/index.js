46752aef3aa1d32b1ac5fbff6a266904a6e6ede0
elizavetachizh <75884592+elizavetachizh@users.noreply.github.com>
2021-11-10 13:27:50 +0300

    feat:  copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,28 +1,37 @@
 const fs = require("fs");
 const path = require("path");
-fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, (err) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-});
-fs.readdir(path.join(__dirname, "files"), (err, files) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-
-  for (let i = 0; i < files.length; i++) {
-    fs.copyFile(
-      path.join(__dirname, "files", files[i]),
-      path.join(__dirname, "files-copy", files[i]),
-      (err) => {
-        if (err) {
-          console.error(err);
-          return;
-        }
-        console.log("Файл из папки успешно скопирован");
+fs.rm(
+  path.join(__dirname, "files-copy"),
+  { recursive: true, force: true },
+  (err) => {
+    if (err) {
+      return console.error(err);
+    }
+    fs.mkdir(path.join(__dirname, "files-copy"), { recursive: true }, (err) => {
+      if (err) {
+        console.error(err);
+        return;
       }
-    );
+    });
+    fs.readdir(path.join(__dirname, "files"), (err, files) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      for (let i = 0; i < files.length; i++) {
+        fs.copyFile(
+          path.join(__dirname, "files", files[i]),
+          path.join(__dirname, "files-copy", files[i]),
+          (err) => {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log("Файл из папки успешно скопирован");
+          }
+        );
+      }
+    });
   }
-});
+);
